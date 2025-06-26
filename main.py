580337594fd2c2b41564d583079374b1750d0084
German <gchistopashin@mail.ru>
2025-06-26 20:21:22 +0300

    new drower

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import csv
-from Visualizer.Drower import load_map, pygame, screen, draw_map, WHITE
+from Visualizer.Drower import Drower
+import pygame
 
 def main():
-    walls = load_map('res/map.csv')  # Убедитесь, что файл в той же директории
+    drower = Drower(800, 600)
+    walls = drower.load_map('res/map.csv')  # Убедитесь, что файл в той же директории
     
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
-        screen.fill(WHITE)
-        draw_map(walls)
-        pygame.display.flip()
+    
+        drower.draw_map(walls)
+        drower.flip()
     
     pygame.quit()
 
